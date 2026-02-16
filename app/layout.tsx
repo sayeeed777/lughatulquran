884fa5ad2e457b92ee2b80935d860c34f45eb5ea
MD Amin Al Sayeed <mdaminalsayeed@MDs-Mac-mini.local>
2026-02-16 13:01:55 +0600

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,12 +71,14 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "96x96" },
-      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
       { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
-      { url: "/icons/icon.svg", type: "image/svg+xml" }
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/icons/icon.svg", type: "image/svg+xml" },
+      { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" }
     ],
-    shortcut: [{ url: "/favicon.ico" }],
+    shortcut: [{ url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" }],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
     ]
